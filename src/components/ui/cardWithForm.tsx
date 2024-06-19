dc10c959230145ba64a64ab6cc1eb6baa4402cc9
yudhonowitanto.yw <yudhonowitanto.yw@gmail.com>
2024-06-19 22:21:56 +0700

    adjust form styling

--- src/components/ui/cardWithForm.tsx
+++ src/components/ui/cardWithForm.tsx
@@ -23,6 +23,8 @@ import {
   SelectValue,
 } from "@/src/components/ui/select";
 import { Textarea } from "@/src/components/ui/textarea";
+import LoadingSpinner from "./loading-spinner";
+
 import { resolver } from "@/src/lib/form-resolvers";
 import { IFormInput } from "@/src/lib/form-types";
 
@@ -83,7 +85,7 @@ export function CardWithForm() {
       });
   };
 
-  console.log(936123, "errors", errors);
+  console.log(8773123, "errors", errors);
 
   return (
     <Card className="w-[350px]">
@@ -173,97 +175,101 @@ export function CardWithForm() {
                 </p>
               )}
             </div>
-            <div className="flex flex-col space-y-1.5">
-              {status === "MAHASISWA" && (
-                <>
-                  <Label htmlFor="instance">Kampus</Label>
-                  <Controller
-                    name="instance"
-                    control={control}
-                    render={({ field }) => (
-                      <Select
-                        {...field}
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <SelectTrigger id="instance">
-                          <SelectValue placeholder="Pilih Kampus" />
-                        </SelectTrigger>
-                        <SelectContent position="popper">
-                          <SelectItem value="UB">
-                            Universitas Brawijaya Malang
-                          </SelectItem>
-                          <SelectItem value="UM">
-                            Universitas Negeri Malang
-                          </SelectItem>
-                          <SelectItem value="UMM">
-                            Universitas Muhammadiyah Malang
-                          </SelectItem>
-                          <SelectItem value="UIN">
-                            Universitas Islam Negeri Malang
-                          </SelectItem>
-                          <SelectItem value="UNISMA">
-                            Universitas Islam Malang
-                          </SelectItem>
-                          <SelectItem value="POLINEMA">
-                            Polteknik Negeri Malang
-                          </SelectItem>
-                        </SelectContent>
-                      </Select>
-                    )}
-                  />
-                  {errors?.instance?.type === "required" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
-                      {errors?.instance?.message}
-                    </p>
+
+            {status === "MAHASISWA" && (
+              <>
+                <Label htmlFor="instance" className="-mb-3">
+                  Kampus
+                </Label>
+                <Controller
+                  name="instance"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger id="instance">
+                        <SelectValue placeholder="Pilih Kampus" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        <SelectItem value="UB">
+                          Universitas Brawijaya Malang
+                        </SelectItem>
+                        <SelectItem value="UM">
+                          Universitas Negeri Malang
+                        </SelectItem>
+                        <SelectItem value="UMM">
+                          Universitas Muhammadiyah Malang
+                        </SelectItem>
+                        <SelectItem value="UIN">
+                          Universitas Islam Negeri Malang
+                        </SelectItem>
+                        <SelectItem value="UNISMA">
+                          Universitas Islam Malang
+                        </SelectItem>
+                        <SelectItem value="POLINEMA">
+                          Polteknik Negeri Malang
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
                   )}
-                </>
-              )}
-              {["OTHER_STATUS", "BEKERJA"].includes(status) && (
-                <>
-                  <Label htmlFor="instance">Instansi</Label>
-                  <Controller
-                    name="instance"
-                    control={control}
-                    render={({ field }) => <Input id="instance" {...field} />}
-                  />
-                  {errors?.instance?.type === "required" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
-                      {errors?.instance?.message}
-                    </p>
+                />
+                {errors?.instance?.type === "required" && (
+                  <p className="text-left text-red-500 font-normal text-sm -mt-3">
+                    {errors?.instance?.message}
+                  </p>
+                )}
+              </>
+            )}
+            {["OTHER_STATUS", "BEKERJA"].includes(status) && (
+              <>
+                <Label htmlFor="instance" className="-mb-3">
+                  Instansi
+                </Label>
+                <Controller
+                  name="instance"
+                  control={control}
+                  render={({ field }) => <Input id="instance" {...field} />}
+                />
+                {errors?.instance?.type === "required" && (
+                  <p className="text-left text-red-500 font-normal text-sm -mt-3">
+                    {errors?.instance?.message}
+                  </p>
+                )}
+                {errors?.instance?.type === "minLength" && (
+                  <p className="text-left text-red-500 font-normal text-sm -mt-3">
+                    {errors?.instance?.message}
+                  </p>
+                )}
+              </>
+            )}
+            {["MAHASISWA"].includes(status) && (
+              <>
+                <Label htmlFor="faculty_or_organitation" className="-mb-3">
+                  Fakultas/Organisasi
+                </Label>
+                <Controller
+                  name="faculty_or_organitation"
+                  control={control}
+                  render={({ field }) => (
+                    <Input id="faculty_or_organitation" {...field} />
                   )}
-                  {errors?.instance?.type === "minLength" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
-                      {errors?.instance?.message}
-                    </p>
-                  )}
-                </>
-              )}
-              {["MAHASISWA"].includes(status) && (
-                <>
-                  <Label htmlFor="faculty_or_organitation">
-                    Fakultas/Organisasi
-                  </Label>
-                  <Controller
-                    name="faculty_or_organitation"
-                    control={control}
-                    render={({ field }) => (
-                      <Input id="faculty_or_organitation" {...field} />
-                    )}
-                  />
-                  {errors?.faculty_or_organitation?.type === "required" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
-                      {errors?.faculty_or_organitation?.message}
-                    </p>
-                  )}
-                  {errors?.faculty_or_organitation?.type === "minLength" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
-                      {errors?.faculty_or_organitation?.message}
-                    </p>
-                  )}
-                </>
-              )}
-            </div>
+                />
+                {errors?.faculty_or_organitation?.type === "required" && (
+                  <p className="text-left text-red-500 font-normal text-sm -mt-3">
+                    {errors?.faculty_or_organitation?.message}
+                  </p>
+                )}
+                {errors?.faculty_or_organitation?.type === "minLength" && (
+                  <p className="text-left text-red-500 font-normal text-sm -mt-3">
+                    {errors?.faculty_or_organitation?.message}
+                  </p>
+                )}
+              </>
+            )}
+
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="participant_type">Datang sebagai?</Label>
               <Controller
@@ -341,12 +347,12 @@ export function CardWithForm() {
                     )}
                   />
                   {errors?.intention_desc?.type === "required" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
+                    <p className="text-left text-red-500 font-normal text-sm -mt-3">
                       {errors?.intention_desc?.message}
                     </p>
                   )}
                   {errors?.intention_desc?.type === "minLength" && (
-                    <p className="text-left text-red-500 font-normal text-sm">
+                    <p className="text-left text-red-500 font-normal text-sm -mt-3">
                       {errors?.intention_desc?.message}
                     </p>
                   )}
@@ -359,25 +365,7 @@ export function CardWithForm() {
           <Button type="submit" className="w-full">
             {isLoading ? (
               <>
-                <div role="status">
-                  <svg
-                    aria-hidden="true"
-                    className="inline w-5 h-5 text-gray-200 animate-spin dark:text-blue-600 fill-blue-600 dark:fill-gray-300"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
-                  <span className="sr-only">Loading...</span>
-                </div>
+                <LoadingSpinner />
               </>
             ) : (
               "Submit"
