0e6fdc4ebf099c0dbc63ce1b97b8ebf9b8216c47
yudhono witanto <yudhonowitanto.yw@gmail.com>
2024-11-22 16:05:14 +0700

    testing

--- app/tes/page.tsx
+++ app/tes/page.tsx
@@ -1,18 +1,25 @@
 'use client'// page.tsx
-import Link from "next/link";
-import { redirect } from "next/navigation";
+// import Link from "next/link";
+// import { redirect } from "next/navigation";
 import { FC, useEffect } from "react";
 
 const LandingPage: FC = () => {
   useEffect(() => {
-    // Replace this with your app's Play Store link
-    const playStoreLink =
+    const deepLinkUrl = "myapp://sscm-presences-webapp.vercel.app/";
+    const playStoreUrl =
       "https://play.google.com/store/apps/details?id=hk.telin.m21";
 
-    // Redirect to Play Store after a delay (optional)
-    window.location.href = playStoreLink;
+    const openApp = () => {
+      window.location.href = deepLinkUrl;
+
+      setTimeout(() => {
+        window.location.href = playStoreUrl;
+      }, 2000); // Adjust the timeout as needed
+    };
+
+    openApp();
   }, []);
-  return redirect("myapp://sscm-presences-webapp.vercel.app/");
+  return <h1>Redirecting...</h1>;
 };
 
 export default LandingPage;
